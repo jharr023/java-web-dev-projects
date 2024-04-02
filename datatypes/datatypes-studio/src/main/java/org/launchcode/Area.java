e8b7c648ed1f9deb9f6098a779bb6887db3b33fc
jharr023 <jharringtonkc@gmail.com>
2024-04-01 20:10:54 -0500

    commit

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,24 @@
+package org.launchcode;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the radius
+        System.out.print("Enter a radius: ");
+        double radius = scanner.nextDouble();
+
+        // Calculate the area using the formula A = pi * r * r
+        double area = Circle.getArea(radius);
+
+        // Print the result
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
+        // Close the scanner to avoid resource leak
+        scanner.close();
+    }
+}
+
